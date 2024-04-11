efa944c073c45a1c1e764bf81967df3e75910d07
KWAME <antwi1496@hotmail.com>
2024-04-11 13:12:43 -0500

    first stages of a vault contract

--- src/Vault.sol
+++ src/Vault.sol
@@ -3,7 +3,14 @@ pragma solidity 0.8.20;
 
 import {IERC20} from "forge-std/interfaces/IERC20.sol";
 
+// Amount/Assets/AmountAssets all refer to the amount of the tokens used in making txns in this vault
+// whenever we want real to deposit or withdraw it is in this currency// i don't know why i find this hard to assimilate
+
 contract Vault {
+    event DepositAsset(address user, uint256 amountAssets);
+    event WithdrawAsset(address user, uint256 amountAsset);
+    event Minted(address to, uint256 amount);
+    event Burned(address fromwhich, uint256 shares);
     // token that the vault uses
     IERC20 immutable token;
 
@@ -16,48 +23,41 @@ contract Vault {
         // initializing the token we want to use as vault tokens
         token = IERC20(_token);
         //there should always be an initial deposit before using this contract
-        uint256 initialSupply = 1000;
+        uint256 initialSupply = 10000;
         totalSupply = initialSupply;
     }
 
     /**
-     * function to mint shares of token to address
+     *  to mint shares of token to address
      * @param to address receiving minted shares
      * @param shares amount of shares
      */
     function mint(address to, uint256 shares) internal {
         require(shares > 0);
+        emit Minted(to, shares);
         totalSupply += shares;
         balanceOf[to] += shares;
     }
 
     /**
-     * function responsible for burning shares of tokens
+     *  responsible for burning shares of tokens
      * @param from address to burn shares from
      * @param shares amount of shares to burn from
      */
     function burn(address from, uint shares) internal {
         require(shares > 0);
+        emit Burned(from, shares);
         totalSupply -= shares;
         balanceOf[from] -= shares;
     }
 
+    /**
+     * converts some amount ot token to shares
+     * @param amount amount of underlying token/asset you want to convert to shares
+     */
     function convertShares(
         uint256 amount
-    ) private view returns (uint256 shares) {
-        shares = (amount * totalSupply) / token.balanceOf(address(this));
-        return shares;
-    }
-
-    function convertAssets(
-        uint256 shares
-    ) private view returns (uint256 amountAssets) {
-        amountAssets = (shares * token.balanceOf(address(this))) / totalSupply;
-        return amountAssets;
-    }
-
-    function Deposit(uint256 amount) public {
-        require(amount > 0);
+    ) internal view returns (uint256 shares) {
         /*
         a = amount
         B = balance of token before deposit
@@ -68,18 +68,59 @@ contract Vault {
 
         s = aT / B
         */
+        amount = amount * 1e8;
+        shares = (amount * totalSupply) / token.balanceOf(address(this));
+        shares = shares / 1e8;
+        return shares;
+    }
+
+    /**
+     * converts shares to assets
+     * @param shares number of shares you want to convert to underlying token/asset
+     */
+    function convertAssets(
+        uint256 shares
+    ) internal view returns (uint256 amountAssets) {
+        /*
+        a = amount
+        B = balance of token before withdraw
+        T = total supply
+        s = shares to burn
+
+        (T - s) / T = (B - a) / B 
+
+        a = sB / T
+        */
+        amountAssets =
+            ((shares * token.balanceOf(address(this))) * 1e8) /
+            totalSupply;
+        return amountAssets / 1e8;
+    }
+
+    /**
+     *A function to deposit assets or amount of some tokens into the vault
+     * @param amount amount of tokens you want to deposit in the vault
+     */
+    function Deposit(uint256 amount) public {
+        require(amount > 0);
         uint256 shares;
         shares = convertShares(amount);
+        emit DepositAsset(msg.sender, amount);
         mint(msg.sender, shares);
         token.transferFrom(msg.sender, address(this), amount);
     }
 
-    function Withdraw(uint256 shares) public returns (uint256) {
+    /**
+     *  to withdraw you shares in the vault
+     * @param shares number of shares you withdrawing
+     */
+    function Withdraw(uint256 shares) public returns (uint256 amount) {
         require(shares > 0);
         require(shares <= balanceOf[msg.sender]);
 
-        uint256 amount = convertAssets(shares);
+        amount = convertAssets(shares);
 
+        emit WithdrawAsset(msg.sender, shares);
         burn(msg.sender, amount);
         token.transferFrom(address(this), msg.sender, amount);
         //returns amount burnt or sent to receiver
